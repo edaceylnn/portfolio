bd05d8c279e380fc35d159ba83fa3bf484834c6d
Eda Ceylan <62337318+edaceylnn@users.noreply.github.com>
2023-06-13 21:12:10 +0300

    Add files via upload

--- src/pages/index.js
+++ src/pages/index.js
@@ -46,7 +46,7 @@ export default function Home() {
               </p>
               <div className="flex items-center self-start mt-2 lg:self-center">
                 <Link
-                  href="/resume.pdf"
+                  href="/Eda Ceylan resume.pdf"
                   target={"_blank"}
                   className="flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold
                   hover:bg-light hover:text-dark border-2 border-solid border-transparent hover:border-dark md:p-2 md:px-4 md:text-base"
